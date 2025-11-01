052984539a7d1eeacf8e6c73b51a5dbe06a63ea0
BickeyManandhar <manandharbickey@gmail.com>
2025-11-01 14:09:25 -0400

    BICKEY-11-1-2025 learning angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-project';
+  title = 'angular-project is my first Angular project.';
+  name = 'Bickey Manandhar';
 }
